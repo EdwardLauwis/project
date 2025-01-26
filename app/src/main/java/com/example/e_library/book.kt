c33f920a969059a6e3ec91cb27ccfda0ec8ccc06
Edward Lauwis <edward.lauwis@binus.ac.id>
2025-01-26 16:59:55 +0700

    new version

--- app/src/main/java/com/example/e_library/book.kt
+++ app/src/main/java/com/example/e_library/book.kt
@@ -1,7 +1,7 @@
 package com.example.e_library
 
-//data class book (
-//    val title: String,
-//    val year: Int,
-//    val genre: String
-//)
+data class book (
+    val title: String,
+    val year: Int,
+    val genre: String
+)
